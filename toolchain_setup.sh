924dc9f243af9a32596cc385b659eac849182be3
ycwan9 <ycwan9@outlook.com>
2019-05-01 15:49:32 +0800

    another error in tar cmd

--- toolchain_setup.sh
+++ toolchain_setup.sh
@@ -10,6 +10,6 @@ ARCHIVE_URL="http://downloads.linksys.com/downloads/gpl/EA6700_v1.1.41.183873_GP
 HND_LOC="publication/src/arm-brcm-linux-uclibcgnueabi/hndtools-arm-linux-2.6.36-uclibc-4.5.3.tar.bz2"
 BUILDROOT_LOC="hndtools-arm-linux-2.6.36-uclibc-4.5.3/src/buildroot-2012.02.tar.bz2"
 wget "$ARCHIVE_URL" -O /tmp/gpl_archive.tgz --progress=dot:giga
-tar -C /tmp -zxvf /tmp/gpl_archive.tgz /tmp/$HND_LOC
+tar -C /tmp -zxvf /tmp/gpl_archive.tgz $HND_LOC
 tar -C /opt -jxvf /tmp/$HND_LOC
 mv /opt/hndtools-arm-linux-2.6.36-uclibc-4.5.3 $TOOLCHAIN_PATH
